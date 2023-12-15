4fdc9c9d0d8e8afb74417733ba99b9947bc0a126
Luigi Consentino <luigi.aconsentino@gmail.com>
2023-12-14 22:39:02 -0300

    create project

---
+++ sistema-estacionamento/Estacionamento.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistema_estacionamento
+{
+    public class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal newPrecoInicial, decimal newPrecoPorHora)
+        {
+            precoInicial = newPrecoInicial;
+            precoPorHora = newPrecoPorHora;
+        }
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do seu veículo para cadastrarmos: ");
+            string placa = Console.ReadLine();
+
+            veiculos.Add(placa);
+
+            Console.WriteLine();
+            Console.WriteLine("Veículo cadastrado.");
+
+            Console.WriteLine();
+        }
+
+        public void ConsultarVeiculoCadastrado()
+        {
+            string placa = "";
+            Console.WriteLine("Qual a placa do veículo?");
+            placa = Console.ReadLine();
+
+            Console.WriteLine();
+            
+            if (veiculos.Any(veiculo => veiculo.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine($"O veículo de placa: {placa} está cadastrado!");
+            }
+            else
+            {
+                Console.WriteLine($"O veículo de placa {placa} não está cadastrado em nosso sistema.");
+            }
+
+            Console.WriteLine();
+        }
+
+        public void ListarVeiculos()
+        {
+
+            int contador = 0;
+
+            Console.WriteLine($"Os veículos cadastrados são: ");
+
+            foreach (string veiculo in veiculos)
+            {
+                contador++;
+                Console.WriteLine($"{contador} - {veiculo}");
+            }
+
+            Console.WriteLine();
+        }
+
+        public void RemoverVeiculo()
+        {
+            string placa = "";
+            Console.WriteLine("Qual a placa do veículo que você deseja remover? ");
+            placa = Console.ReadLine();
+
+            Console.WriteLine();
+
+            if (veiculos.Any(veiculo => veiculo.ToUpper() == placa.ToUpper()))
+            {
+                veiculos.Remove(placa);
+                Console.WriteLine($"Veículo de placa: {placa} foi removido com sucesso.");
+            }
+            else
+            {
+                Console.WriteLine($"O veículo de placa: {placa} não está cadastrado em nosso sistema.");
+            }
+
+            Console.WriteLine();
+        }
+
+        public void CalculoValorVeiculo(decimal precoInicial, decimal precoPorHora)
+        {
+            decimal calculo = 0;
+            decimal quantidadeHoras = 0;
+
+            Console.WriteLine("Quantas horas o veículo ficou no estacionamento? ");
+            quantidadeHoras = Convert.ToDecimal(Console.ReadLine());
+
+            calculo = precoInicial + (quantidadeHoras * precoPorHora);
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Valor total a se pagar: {calculo}");
+
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ sistema-estacionamento/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+using sistema_estacionamento;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        decimal definirPrecoInicial = 0;
+        decimal definirPrecoPorHora = 0;
+        int resposta;
+
+        Console.WriteLine("Define o preço inicial a ser pago: ");
+        definirPrecoInicial = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine();
+
+        Console.WriteLine("Define o preço a ser pago por hora: ");
+        definirPrecoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine();
+
+        Console.WriteLine("Seja Bem Vindo ao Estacionamento Local Bacana!\n" +
+            "Deseja prosseguir? \n" +
+            "Digite: 1 - Sim, 2 - Não");
+        resposta = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();
+
+
+        Estacionamento estacionamento = new Estacionamento(definirPrecoInicial, definirPrecoPorHora);
+        
+        while(resposta == 1) {
+            Console.WriteLine("Digite a sua opção:");
+            Console.WriteLine("1 - Cadastrar veículo");
+            Console.WriteLine("2 - Remover veículo");
+            Console.WriteLine("3 - Listar veículos");
+            Console.WriteLine("4 - Consultar se veículo está cadastrado");
+            Console.WriteLine("5 - Calcular valor total a se pagar");
+            Console.WriteLine("6 - Encerrar");
+
+            switch(Console.ReadLine())
+            {
+                case "1":
+                    estacionamento.AdicionarVeiculo();
+                    break;
+                
+                case "2":
+                    estacionamento.RemoverVeiculo();
+                    break;
+
+                case "3":
+                    estacionamento.ListarVeiculos();
+                    break;
+
+                case "4":
+                    estacionamento.ConsultarVeiculoCadastrado();
+                    break;
+
+                case "5":
+                    estacionamento.CalculoValorVeiculo(definirPrecoInicial, definirPrecoPorHora);
+                    break;
+
+                case "6":
+                    Console.WriteLine("Sistema encerrado, Volte Sempre!");
+                    break;
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Seja Bem Vindo ao Estacionamento Local Bacana!\n" +
+            "Deseja prosseguir? \n" +
+            "Digite: 1 - Sim, 2 - Não");
+            resposta = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+        }
+
+        
+    }
+}
+
